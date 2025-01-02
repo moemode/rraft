4eef38c262a42c9259540fdfd1f7cdb4874890ec
moemode <pfeffer.moritz@gmail.com>
2025-01-02 16:41:36 +0000

    Cleanup raft and work on follower

--- rraft/src/bin/raft.rs
+++ rraft/src/bin/raft.rs
@@ -1,11 +1,3 @@
-// implement two roles a responder which echoes messages
-// and a emitter which every second sends a message to the responder
-// when the responder receives a message with the data "emit" it will
-// itself become an emitter for two seconds
-
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
-use std::time::Duration;
 use std::time::Instant;
 
 #[derive(Debug)]
@@ -18,18 +10,6 @@ struct State {
     commit_index: usize,
     last_applied: usize,
     n_nodes: usize,
-}
-
-#[derive(Debug)]
-struct RequestMessage {
-    id: u32,
-    data: String,
-}
-
-#[derive(Debug)]
-struct ResponseMessage {
-    id: u32,
-    data: String,
 }
 
 #[derive(Debug)]
@@ -48,6 +28,19 @@ enum Message {
         success: bool,
         from: usize,
     },
+
+    RequestVoteRequest {
+        term: usize,
+        candidate_id: usize,
+        last_log_index: usize,
+        last_log_term: usize,
+    },
+
+    RequestVoteResponse {
+        term: usize,
+        vote_granted: bool,
+        from: usize,
+    },
 }
 
 trait HasTerm {
@@ -59,36 +52,9 @@ impl HasTerm for Message {
         match self {
             Message::AppendEntryRequest { term, .. } => *term,
             Message::AppendEntryResponse { term, .. } => *term,
+            Message::RequestVoteRequest { term, .. } => *term,
+            Message::RequestVoteResponse { term, .. } => *term,
         }
-    }
-}
-
-#[derive(Debug)]
-struct TimedMessage {
-    delivery_time: Instant,
-    from: u32,
-    to: u32,
-    message: Message,
-}
-
-impl PartialEq for TimedMessage {
-    fn eq(&self, other: &Self) -> bool {
-        self.delivery_time == other.delivery_time
-    }
-}
-
-impl Eq for TimedMessage {}
-
-impl PartialOrd for TimedMessage {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for TimedMessage {
-    fn cmp(&self, other: &Self) -> Ordering {
-        // Reverse ordering for min-heap (earliest messages first)
-        other.delivery_time.cmp(&self.delivery_time)
     }
 }
 
@@ -97,11 +63,10 @@ trait Role {
     fn handle(&mut self, msg: Message, at: Instant, s: &mut State) -> Vec<Message>;
     fn tick(&mut self, at: Instant, s: &mut State) -> Option<Box<dyn Role>>;
     fn tick_msg(&mut self, at: Instant, s: &mut State) -> Vec<Message>;
-    fn id(&self) -> u32;
 }
 
 struct Candidate {
-    votes_received: usize,
+    votes_received: HashSet<usize>,
     election_started: Instant,
 }
 
@@ -112,8 +77,26 @@ impl Role for Candidate {
             s.current_term = term;
             return Some(Box::new(Follower {}));
         }
-
-        None
+        match msg {
+            Message::AppendEntryRequest { term, .. } => {
+                if *term >= s.current_term {
+                    return Some(Box::new(Follower {}));
+                }
+                None
+            }
+            Message::RequestVoteResponse {
+                vote_granted, from, ..
+            } => {
+                if *vote_granted {
+                    self.votes_received.insert(*from);
+                    if self.votes_received.len() > (s.n_nodes / 2) + 1 {
+                        return Some(Box::new(Leader::new()));
+                    }
+                }
+                None
+            }
+            _ => None,
+        }
     }
 
     fn handle(&mut self, msg: Message, at: Instant, s: &mut State) -> Vec<Message> {
@@ -177,89 +160,5 @@ impl Machine {
 
     fn id(&self) -> u32 {
         self.role.id()
-    }
-}
-
-fn main() {
-    let mut message_queue: BinaryHeap<TimedMessage> = BinaryHeap::new();
-    let start_time = Instant::now();
-    let mut current_time = start_time;
-
-    // Create machines: emitter sends pings, responder echoes
-    let mut emitter = Machine::new(Box::new(Emitter::new(1, current_time, None)));
-    let mut responder = Machine::new(Box::new(Responder::new(2)));
-
-    // Simulation parameters
-    let sim_duration = Duration::from_secs(30);
-    let tick_interval = Duration::from_millis(10);
-    let network_latency = Duration::from_millis(50); // Simulated network delay
-
-    while current_time - start_time < sim_duration {
-        // Process messages that have arrived
-        while let Some(timed_msg) = message_queue.peek() {
-            if timed_msg.delivery_time > current_time {
-                break;
-            }
-
-            let msg = message_queue.pop().unwrap();
-            println!(
-                "Time {:?} - Delivering message from {} to {}: {:?}",
-                current_time - start_time,
-                msg.from,
-                msg.to,
-                msg.message
-            );
-
-            let responses = if msg.to == emitter.id() {
-                emitter.handle(msg.message, current_time)
-            } else {
-                responder.handle(msg.message, current_time)
-            };
-
-            // Queue responses with network latency
-            for response in responses {
-                message_queue.push(TimedMessage {
-                    delivery_time: current_time + network_latency,
-                    from: msg.to,
-                    to: msg.from,
-                    message: response,
-                });
-            }
-        }
-
-        // Tick both machines
-        for msg in emitter.tick(current_time) {
-            println!(
-                "Time {:?} - Emitter generated message: {:?}",
-                current_time - start_time,
-                msg
-            );
-            message_queue.push(TimedMessage {
-                delivery_time: current_time + network_latency,
-                from: emitter.id(),
-                to: responder.id(),
-                message: msg,
-            });
-        }
-
-        for msg in responder.tick(current_time) {
-            println!(
-                "Time {:?} - Responder generated message: {:?}",
-                current_time - start_time,
-                msg
-            );
-            message_queue.push(TimedMessage {
-                delivery_time: current_time + network_latency,
-                from: responder.id(),
-                to: emitter.id(),
-                message: msg,
-            });
-        }
-        // Sleep until next tick
-        current_time += tick_interval;
-        // Optional: print queue status every second
-        /*         if (current_time - start_time).as_secs() % 1 == 0 {
-            println!("Queue size: {}", message_queue.len());
-        } */
     }
 }
